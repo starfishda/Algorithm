6ecd4ca1baba2b09cca11a8114010a0912da3384
starfishda <32675267+starfishda@users.noreply.github.com>
2020-03-28 01:12:36 +0900

    Create BOJ - 11722 (가장 긴 감소하는 부분 수열).cpp

---
+++ BOJ/DP/BOJ - 11722 (가장 긴 감소하는 부분 수열).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+int main() {
+	int n;
+	int dp[1000] = {};
+	int arr[1000] = {};
+	int sum = 0;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		dp[i] = 1;
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[i] < arr[j])
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+		sum = max(sum, dp[i]);
+	}
+
+	cout << sum << endl;
+	return 0;
+}
