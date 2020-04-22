13ee3d869110c7c2fbb17478888501197fcd63e0
starfishda <32675267+starfishda@users.noreply.github.com>
2020-04-23 00:07:18 +0900

    Update BOJ - 1932 (정수 삼각형).cpp

--- BOJ/DP/BOJ - 1932 (정수 삼각형).cpp
+++ BOJ/DP/BOJ - 1932 (정수 삼각형).cpp
@@ -1,40 +1,36 @@
 #include <iostream>
 #include <algorithm>
-#include<math.h>
+#include<string>
 using namespace std;
-
-
+int tri[501][501];
 int dp[501][501];
 
 int main() {
 	int n;
-
 	cin >> n;
-
+	
 	for (int i = 1; i <= n; i++) {
-		for (int j = 1; i <= i; j++) {
-			cin >> dp[i][j];
+		for (int j = 1; j <= i; j++) {
+			cin >> tri[i][j];
 		}
 	}
-
+	dp[1][1] = tri[1][1];
 	for (int i = 2; i <= n; i++) {
 		for (int j = 1; j <= i; j++) {
 			if (j == 1)
-				dp[i][j] += dp[i - 1][j];
-			else if (j == n)
-				dp[i][j] += dp[i - 1][j - 1];
-			else
-				dp[i][j] += max(dp[i - 1][j], dp[i - 1][j - 1]);
-
+				dp[i][j] = dp[i - 1][j] + tri[i][j];
+			else if (j == i)
+				dp[i][j] = dp[i - 1][j - 1] + tri[i][j];
+			else 
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + tri[i][j];
 		}
 	}
 
-	int sum = 0;
-	for (int i = 0; i <= n; i++) {
-		if (dp[n][i] > sum)
-			sum = dp[n][i];
+	int ans = 0;
+	for (int i = 1; i <= n; i++) {
+		ans = max(ans, dp[n][i]);
 	}
 
-	cout << sum << endl;
+	cout << ans;
 	return 0;
 }
