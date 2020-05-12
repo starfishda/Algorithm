a09e9b32e0a6394292d172e42bc35ec37a625795
starfishda <32675267+starfishda@users.noreply.github.com>
2020-05-13 00:00:12 +0900

    Create BOJ - 15666 (N과 M(12)).cpp

---
+++ BOJ/Brute Force/BOJ - 15666 (N과 M(12)).cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+int n, m;
+int ans[10];
+int num[10];
+
+void recur(int tmp, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << ans[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	bool check[10002] = { 0, };
+	for (int i = tmp; i <= n; i++) {
+		if (!check[num[i]]) {
+			ans[cnt] = num[i];
+			check[num[i]] = true;
+			recur(i, cnt + 1);
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+
+	for (int i = 1; i <= n; i++)
+		cin >> num[i];
+	sort(num + 1, num + n + 1);
+
+	recur(1, 0);
+
+	return 0;
+}
