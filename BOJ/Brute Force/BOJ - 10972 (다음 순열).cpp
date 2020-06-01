fd2cc4919ea1140cfe1ae5df88a0625e5821d032
starfishda <32675267+starfishda@users.noreply.github.com>
2020-06-02 00:00:38 +0900

    Create BOJ - 10972 (다음 순열).cpp

---
+++ BOJ/Brute Force/BOJ - 10972 (다음 순열).cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+
+int main() {
+	int n;
+	cin >> n;
+
+	int *arr = new int[n];
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	if (next_permutation(arr, arr + n))
+		for (int i = 0; i < n; i++)
+			cout << arr[i] << " ";
+	else
+		cout << -1 << "\n";
+
+	return 0;
+}
