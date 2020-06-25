7c59e002eccbd02f27c0c5905ca3f911b54301aa
starfishda <32675267+starfishda@users.noreply.github.com>
2020-06-26 00:30:52 +0900

    Create BOJ - 1991 (트리 순회).cpp

---
+++ BOJ/Tree/BOJ - 1991 (트리 순회).cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+struct node {
+	char left;
+	char right;
+};
+struct node arr[100];
+
+void preorder(char root) {
+	if (root == '.') {
+		return;
+	}
+	else {
+		cout << root;
+		preorder(arr[root].left);
+		preorder(arr[root].right);
+	}
+}
+
+void inorder(char root) {
+	if (root == '.') {
+		return;
+	}
+	else {
+		inorder(arr[root].left);
+		cout << root;
+		inorder(arr[root].right);
+	}
+}
+
+void postorder(char root) {
+	if (root == '.') {
+		return;
+	}
+	else {
+		postorder(arr[root].left);
+		postorder(arr[root].right);
+		cout << root;
+	}
+}
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n;
+	char t1, t2, t3;
+
+	for (int i = 0; i < n; i++) {
+		cin >> t1 >> t2 >> t3;
+		arr[t1].left = t2;
+		arr[t1].right = t3;
+	}
+
+	preorder('A');
+	cout << "\n";
+	inorder('A');
+	cout << "\n";
+	postorder('A');
+}
