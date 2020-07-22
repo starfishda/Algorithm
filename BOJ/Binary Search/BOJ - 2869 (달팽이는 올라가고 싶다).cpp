d159d801a3153dae0aa82f047f10b62df9452586
starfishda <32675267+starfishda@users.noreply.github.com>
2020-07-23 00:01:54 +0900

    Create BOJ - 2869 (달팽이는 올라가고 싶다).cpp

---
+++ BOJ/Binary Search/BOJ - 2869 (달팽이는 올라가고 싶다).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int a, b, v;
+	cin >> a >> b >> v;
+
+	if ((v - b) % (a - b) != 0)
+		cout << (v - b) / (a - b) + 1;
+	else
+		cout << (v - b) / (a - b);
+
+	return 0;
+}
